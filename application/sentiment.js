26952606921faf6d70506384f0a69064fd585eb7
Moritz Horn <moritz.horn@gmail.com>
2015-04-17 14:37:02 +0200

    added first application files

---
+++ application/sentiment.js
@@ -0,0 +1,9 @@
+var twittersentiment = angular.module('TwitterSentiment', ['ui.bootstrap']);
+
+twittersentiment.controller('myController',['$scope', function('$scope'){
+	
+}]);
+twittersentiment.run(function($rootscope, $http){
+	$rootscope.data = [];
+	
+});
