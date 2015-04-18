b53649596b554ceec55712aa5a0520afa152ab14
Niklas <nachtmahr2010@googlemail.com>
2015-04-18 23:29:04 +0200

    Update count.py

--- SentimentAnalysis/count.py
+++ SentimentAnalysis/count.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon Apr 13 17:19:53 2015
+
 @author: Niklas Bendixen
 """
 
 import re                                                                       #import of regex
+import codecs                                                                   #import of utf8 support
 import csv                                                                      #import of csv
+import sys
+import glob
+import string
+import os
 
 #read_neutral = open("germanneutral.tsv", "r")
 #    for line in csv.reader(germanneutral.tsv, dialect="excel-tab")
@@ -22,15 +29,42 @@ writefile = open("output.txt", "w")                                             
 #count_positive = 0
 #count_negative = 0
 
+#for line in readfile:
+#    writefile.write(str(line.replace('\n', '')) + "\t\t\tneutral: " + "\tpositiv: " + "\tnegativ: \n")
 
-#m = re.match(r"^A-Za-z", line)
-    
-for line in readfile:
-    writefile.write(str(line.replace('\n', '')) + "\t\t\tneutral: " + "\tpositiv: " + "\tnegativ: \n")
-    
 
-readfile.close()                                                                #closes opened file
-writefile.close()                                                               #closes opened file
+
+
 #read_neutral.close()                                                           #closes opened file
 #read_positive.close()                                                          #closes opened file
-#read_negative.close()  
+#read_negative.close()                                                          #closes opened file
+readfile.close()                                                                #closes opened file
+writefile.close()                                                               #closes opened file
+
+
+
+
+### Tried and worthless ###
+
+#n = re.search(r"^[\w]+[\s|\t]", readfile, re.multiline)
+#m = re.match(r"^A-Za-z", line)
+#for line in readfile:
+#    writefile.write(str(line.replace('\n', '')) + "\t\t\t" + n)
+    
+    
+#for line in sys.stdin.readlines():
+#for line in readfile.readlines():
+#    if line[0] == 'werde':
+#        print line,
+    
+    
+#for l in readfile.splitlines():
+#    if re.search(r"^\w*[\t|\s]", l):
+#        writefile.write(str(line.replace('\n', '')) + l + \n")    
+
+
+#for line in readfile:
+#    d = re.match("werde", line),
+#if d:
+#    print "true"
+#else print "fail"
