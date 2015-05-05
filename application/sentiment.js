981286366939f7a15bf7b09433535fe38ad3f888
Moritz Horn <moritz.horn@gmail.com>
2015-05-05 10:10:37 +0200

    new apllication functionality

--- application/sentiment.js
+++ application/sentiment.js
@@ -3,8 +3,11 @@ var twitter = angular.module('myApp', ['ui.bootstrap','nvd3ChartDirectives']);
 twitter.controller('myController', function($rootScope, $scope){
 	$scope.view = "";
 	$scope.selectView = function(view){
-		console.log(view);
 		$scope.view = view;
+		$scope.graphData = [];
+		$scope.selectedGraphs = [];
+		$scope.selectedCountries = [];
+		$scope.selectedTopics = [];
 		if(view == ''){
 			$scope.selectedData = {
 				topic: "",
@@ -44,25 +47,59 @@ twitter.controller('myController', function($rootScope, $scope){
 	$scope.showTopics = function(){
 		if(!$scope.topics.length){
 			angular.forEach($scope.data, function(value, key){
-				console.log(value.value);
 				$scope.topics.push(value.value);
 			});
 		}
 	};
+	$scope.colorArray = ['#FF0000', '#0000FF', '#FFFF00', '#00FFFF'];
+	$scope.colorSelect = function() {
+		var colors = $scope.colorArray;
+		return function(d, i) {
+			console.log(colors[i] + "color");
+			return colors[i];
+		};
+	}
+	$scope.$watch('graphData', function(){
+	},true);
+	$scope.graphData = [];
+	$scope.selectedGraphs = [];
+	$scope.selectedCountries = [];
+	$scope.selectedTopics = [];
+	
 	$scope.selectTopic = function(index){
 		$scope.selectedData.topic = {
 			value: $scope.data[index].value,
 			index: index
 		}
+		var duplicate = false;
+		var dpos = "";
 		if($scope.selectedData.country){
-			var cindex = $scope.selectedData.country.index;
-			$scope.selectedData.values = $scope.data[index].content[cindex].rates;
-			$scope.graphData = [
-				{
-					key: "Series 1",
-					values: [[ "positive", $scope.selectedData.values.positive ],[ "neutral", $scope.selectedData.values.neutral ],[ "negative", $scope.selectedData.values.negative ]]
+			var i = 0;
+			angular.forEach($scope.selectedGraphs, function(v, k){
+				if(v.key == $scope.selectedData.topic.value){
+					duplicate = true;
+					dpos = i;
 				}
-			]
+				i++;
+			})
+			if(duplicate == false){
+				var cindex = $scope.selectedData.country.index;
+				$scope.selectedTopics[$scope.selectedData.topic.value] = true;
+				$scope.selectedData.values = $scope.data[index].content[cindex].rates;
+				var rpos = $scope.selectedData.values.positive / ($scope.selectedData.values.positive + $scope.selectedData.values.negative);
+				var rneg = $scope.selectedData.values.negative / ($scope.selectedData.values.positive + $scope.selectedData.values.negative);
+				var data = 	{
+						key: $scope.selectedData.topic.value,
+						values: [[ "positive", rpos ],[ "negative", rneg ]]
+					}
+				console.log(data);
+				$scope.selectedGraphs.push(data);
+				$scope.graphData = angular.copy($scope.selectedGraphs);
+			}else{
+				$scope.selectedTopics[$scope.selectedData.topic.value] = false;
+				$scope.selectedGraphs.splice(dpos, 1);
+				$scope.graphData = angular.copy($scope.selectedGraphs);
+			}
 		}else{
 			$scope.selectView('countries');
 		}
@@ -79,16 +116,34 @@ twitter.controller('myController', function($rootScope, $scope){
 			value: $scope.data[0].content[index].value,
 			index: index
 		}
+		var duplicate = false;
+		var dpos = "";
 		if($scope.selectedData.topic){
-			var tindex = $scope.selectedData.topic.index;
-			$scope.selectedData.values = $scope.data[tindex].content[index].rates;
-			console.log($scope.selectedData.values.positive);
-			$scope.graphData = [
-				{
-					key: "Series 1",
-					values: [[ "positive", $scope.selectedData.values.positive ],[ "neutral", $scope.selectedData.values.neutral ],[ "negative", $scope.selectedData.values.negative ]]
+			var i = 0;
+			angular.forEach($scope.selectedGraphs, function(v, k){
+				if(v.key == $scope.selectedData.country.value){
+					duplicate = true;
+					dpos = i;
 				}
-			]
+				i++;
+			})
+			if(duplicate == false){	
+				var tindex = $scope.selectedData.topic.index;
+				$scope.selectedCountries[$scope.selectedData.country.value] = true;
+				$scope.selectedData.values = $scope.data[tindex].content[index].rates;
+				var data = 	{
+						key: $scope.selectedData.country.value,
+						color:'#FF0000',
+						values: [[ "positive", $scope.selectedData.values.positive ],[ "negative", $scope.selectedData.values.negative ]]
+					}
+				console.log(data);
+				$scope.selectedGraphs.push(data);
+				$scope.graphData = angular.copy($scope.selectedGraphs);
+			}else{
+				$scope.selectedCountries[$scope.selectedData.country.value] = false;
+				$scope.selectedGraphs.splice(dpos, 1);
+				$scope.graphData = angular.copy($scope.selectedGraphs);
+			}
 		}else{
 			$scope.selectView('topics');
 		}
