4ae01bd6baf4d19bab7a2625c299dee8d6d01796
Niklas <nachtmahr2010@googlemail.com>
2015-06-01 11:17:28 +0200

    Ranking Data
    Ranking Data as txt-Files for positive, negative, and all tweets

--- visualization/visual.py
+++ visualization/visual.py
@@ -7,7 +7,9 @@ Created on Tue Apr 28 09:53:01 2015
 import pylab as P
 
 readfile = open("../SentimentAnalysis/sentiment_analysed_tweets.txt", "r")
-ranking_file = open("data_ranking.txt", "w")
+ranking_file_all = open("data_ranking_all.txt", "w")
+ranking_file_pos = open("data_ranking_pos.txt", "w")
+ranking_file_neg = open("data_ranking_neg.txt", "w")
 
 ### Arrays
 countries = []
@@ -55,20 +57,33 @@ for line in readfile:
 #print dict_neg
 #print dict_all
 
-### Table
-ranking_file.write('{0}\t{1}\n'.format('Countries','\t'.join(\
+### Tables
+ranking_file_all.write('{0}\t{1}\n'.format('Countries','\t'.join(\
 [str(j) for j in topics])))
 for i in dict_all.keys():
-    ranking_file.write('{0}\t{1}\n'.format(i,'\t'.join(\
+    ranking_file_all.write('{0}\t{1}\n'.format(i,'\t'.join(\
     [str(j) for j in dict_all[i]])))
+    
+ranking_file_pos.write('{0}\t{1}\n'.format('Countries','\t'.join(\
+[str(j) for j in topics])))
+for i in dict_pos.keys():
+    ranking_file_pos.write('{0}\t{1}\n'.format(i,'\t'.join(\
+    [str(j) for j in dict_pos[i]])))
+
+
+ranking_file_neg.write('{0}\t{1}\n'.format('Countries','\t'.join(\
+[str(j) for j in topics])))
+for i in dict_neg.keys():
+    ranking_file_neg.write('{0}\t{1}\n'.format(i,'\t'.join(\
+    [str(j) for j in dict_pos[i]])))
 
 ### Plotting        
 #x = range(1,len(topics)+1)
 #
-color = ['#FF0000','#0000FF','#FFA500','#000000','#BEBEBE','#00FF00','#8470FF',
-'#A52A2A', '#FFC0CB', '#F5F5DC', '#EEDFCC', '#E6E6FA', '#00BFFF', '#00FF7F',
-'#FFD700', '#CD5C5C', '#FF6347', '#D8BFD8', '#556B2F', '#FFE4E1', '#E0EEE0',
-'#2F4F4F', '#CAFF70', '#DA70D6']
+#color = ['#FF0000','#0000FF','#FFA500','#000000','#BEBEBE','#00FF00','#8470FF',
+#'#A52A2A', '#FFC0CB', '#F5F5DC', '#EEDFCC', '#E6E6FA', '#00BFFF', '#00FF7F',
+#'#FFD700', '#CD5C5C', '#FF6347', '#D8BFD8', '#556B2F', '#FFE4E1', '#E0EEE0',
+#'#2F4F4F', '#CAFF70', '#DA70D6']
 #
 #index = 0
 #fig = plt.figure()
@@ -96,51 +111,51 @@ color = ['#FF0000','#0000FF','#FFA500','#000000','#BEBEBE','#00FF00','#8470FF',
 #plt.show()
 
 ### Histogram
-file_hist = open("data_ranking.txt", "r")
-
-
-hist = []
-
-for line in file_hist:
-    if line[0] not in countries:
-        countries.append(line[2])
-    if line[1] not in topics:
-        topics.append(line[1])
-
-
-dict_hist = {}
-
-for i in countries:
-    dict_hist[i] = [0]*len(topics)
-
-
-
-for line in file_hist:
-    line = line.strip().replace('-','5').replace('+','10')\
-        .replace("\xfc", "ue").replace("\xe4", "ae").split('\t')
+#file_hist = open("data_ranking.txt", "r")
+#
+#
+#hist = []
+#
+#for line in file_hist:
+#    if line[0] not in countries:
+#        countries.append(line[2])
+#    if line[1] not in topics:
+#        topics.append(line[1])
+#
+#
+#dict_hist = {}
+#
+#for i in countries:
+#    dict_hist[i] = [0]*len(topics)
+#
+#
+#
+#for line in file_hist:
+#    line = line.strip().replace('-','5').replace('+','10')\
+#        .replace("\xfc", "ue").replace("\xe4", "ae").split('\t')
 #
 # first create a single histogram
 #
-mu, sigma = 200, 25
-x = mu + sigma*P.randn(10000)
+#mu, sigma = 200, 25
+#x = mu + sigma*P.randn(10000)
 
 # the histogram of the data with histtype='step'
-n, bins, patches = P.hist(x, 50, normed=1, histtype='stepfilled')
-P.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+#n, bins, patches = P.hist(x, 50, normed=1, histtype='stepfilled')
+#P.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
 
 # add a line showing the expected distribution
-y = P.normpdf( bins, mu, sigma)
-l = P.plot(bins, y, 'k--', linewidth=1.5)
-
-P.figure()
+#y = P.normpdf( bins, mu, sigma)
+#l = P.plot(bins, y, 'k--', linewidth=1.5)
+#
+#P.figure()
 
 # create a new data-set
-x = mu + sigma*P.randn(1000,3)
-
-n, bins, patches = P.hist(x, 10, normed=1, histtype='bar',
-                            color=['crimson', 'burlywood', 'chartreuse'],
-                            label=['Crimson', 'Burlywood', 'Chartreuse'])
-P.legend()
+#x = mu + sigma*P.randn(1000,3)
+#
+#n, bins, patches = P.hist(x, 10, normed=1, histtype='bar',
+#                            color=['crimson', 'burlywood', 'chartreuse'],
+#                            label=['Crimson', 'Burlywood', 'Chartreuse'])
+#P.legend()
 
 
 
@@ -151,5 +166,7 @@ P.legend()
 
 
 readfile.close()
-ranking_file.close()
-file_hist.close()
+ranking_file_all.close()
+ranking_file_pos.close()
+ranking_file_neg.close()
+#file_hist.close()
